21317ab218c533696ab646c71558ed56b14abf9c
theabhipatel <theabhipatel.co@gmail.com>
2025-11-08 14:39:35 +0530

    feat: add a component of sparkles which will be used for footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { Github, Linkedin, Mail, Code2, Server, Layers } from "lucide-react";
 import fluidCursor from "@/hooks/use-FluidCursor";
+import { SparklesCore } from "@/components/ui/sparkles";
 
 export default function Page() {
   const [scrollY, setScrollY] = useState(0);
@@ -179,6 +180,37 @@ export default function Page() {
           <p className="text-xl text-gray-400">Amazing work portfolio</p>
         </div>
       </section>
+      <SparklesPreview />
+    </div>
+  );
+}
+
+export function SparklesPreview() {
+  return (
+    <div className="flex h-[40rem] w-full flex-col items-center justify-center overflow-hidden rounded-md bg-black">
+      <h1 className="relative z-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-9xl">
+        TheAbhiPatel
+      </h1>
+      <div className="relative h-40 w-[40rem]">
+        {/* Gradients */}
+        <div className="absolute inset-x-20 top-0 h-[2px] w-3/4 bg-gradient-to-r from-transparent via-indigo-500 to-transparent blur-sm" />
+        <div className="absolute inset-x-20 top-0 h-px w-3/4 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+        <div className="absolute inset-x-60 top-0 h-[5px] w-1/4 bg-gradient-to-r from-transparent via-sky-500 to-transparent blur-sm" />
+        <div className="absolute inset-x-60 top-0 h-px w-1/4 bg-gradient-to-r from-transparent via-sky-500 to-transparent" />
+
+        {/* Core component */}
+        <SparklesCore
+          background="transparent"
+          minSize={0.4}
+          maxSize={1}
+          particleDensity={1200}
+          className="h-full w-full"
+          particleColor="#FFFFFF"
+        />
+
+        {/* Radial Gradient to prevent sharp edges */}
+        <div className="absolute inset-0 h-full w-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+      </div>
     </div>
   );
 }
