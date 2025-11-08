b05820190a3c5f09fda9e44d8af6e0c9c357afd4
theabhipatel <theabhipatel.co@gmail.com>
2025-11-08 14:59:17 +0530

    chore: add comment for a cursor

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,8 @@ export function SparklesPreview() {
   return (
     <div className="flex h-[40rem] w-full flex-col items-center justify-center overflow-hidden rounded-md bg-black">
       <h1 className="relative z-20 text-center text-3xl font-bold text-white md:text-7xl lg:text-9xl">
-        TheAbhiPatel
+        TheAbhiPatel{" "}
+        {/* TheAbhiPatel - make this name with this cursonr https://cursify.vercel.app/components/snowflake-cursor */}
       </h1>
       <div className="relative h-40 w-[40rem]">
         {/* Gradients */}
